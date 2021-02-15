9e0f73ac836962b04c12d6cac7d002b1ea889bc3
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-02-15 21:06:32 +0300

    feat: add slider, script.js

--- script.js
+++ script.js
@@ -6,21 +6,63 @@ const btnLeft = document.querySelector('.btn-left');
 let slides = [];
 let currentPosition = 0;
 
-const createSlide = (parent, content) => {
+const DATA = [
+  {
+    name: 'deer', 
+    image: './assets/images/latest-articles/deer.jpg',
+    text: 'Naturalist investigation'
+  },
+  {
+    name: 'squirrel', 
+    image: './assets/images/latest-articles/squirrel.jpg',
+    text: 'Kamikaze squirrels'
+  },
+  {
+    name: 'bird', 
+    image: './assets/images/latest-articles/bird.jpg',
+    text: 'Birds Fight club'
+  },
+  {
+    name: 'ladybug', 
+    image: './assets/images/latest-articles/ladybug.jpg',
+    text: 'Nice red white dotted Bug'
+  },
+  {
+    name: 'mushroom', 
+    image: './assets/images/latest-articles/mushroom.jpg',
+    text: 'Not little mushroom'
+  },
+  {
+    name: 'wolf', 
+    image: './assets/images/latest-articles/wolf.jpg',
+    text: 'Woooooooooooolf'
+  }
+]
+
+const createSlide = (parent, content, data, index) => {
   const el = document.createElement('div');
   el.className = 'slide';
+  el.style.backgroundImage = `url(${data[index].image})`;
   el.innerHTML = content;
   parent.appendChild(el);
   return el;
 }
 
-const html = `
-  <h4>Deer</h4>
-`
+const createHTML = (data, index) => {
 
-for (let i = 1; i < 10; i++) {
-  slides.push(createSlide(slidesContainer, html));
-  }
+  return (
+    ` <div class="slide__inner">
+        <h4 class="slide__inner__caption">${data[index].name}</h4>
+        <p class="slide__inner__text">${data[index].text}</p>
+      </div> 
+    `
+  )
+} 
+
+
+for (let i = 0; i < 6; i++) {
+  slides.push(createSlide(slidesContainer, createHTML(DATA, i), DATA, i));
+}
 
 btnLeft.addEventListener('click', () => {
   setPosition(currentPosition + 100);
@@ -37,5 +79,8 @@ const setPosition = (position) => {
     return false;
   }
   currentPosition = position;
-  slides.forEach((slide) => slide.style.transform = `translateX(${currentPosition}%)`);
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(${currentPosition}%)`;
+  });
 }
+
